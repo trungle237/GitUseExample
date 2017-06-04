f16aa6d2deea06fb207118597936a1646d69ba54
Trung Le <trungle@trungs-mac-mini.net.fpt>
2017-06-05 00:01:20 +0700

    add a button and press this button a message will appear

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -11,9 +11,14 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+                super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        print("This is a Git tutorial")
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
